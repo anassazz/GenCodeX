ff67c5f2917a36070fcdc18384ad437164ccba8f
anassazz <anass.azz2030@gmail.com>
2025-05-20 12:35:42 +0100

    Initialize App component with basic structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col items-center py-10 px-4">
+      
+    </div>
+  );
+}
+
+export default App;
